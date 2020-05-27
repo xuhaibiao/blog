ab99af4c7bd38c8d4d18a7cb07ef853af5ebc5b2
xuhaibiao <854186712@qq.com>
2020-05-27 17:41:08 +0800

    【update】新增网页图标，修改颜色内容等信息

--- src/main/java/cn/xhb/blog/BlogApplication.java
+++ src/main/java/cn/xhb/blog/BlogApplication.java
@@ -1,11 +1,12 @@
 package cn.xhb.blog;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class BlogApplication {
 
+@SpringBootApplication
+public class BlogApplication{
     public static void main(String[] args) {
         SpringApplication.run(BlogApplication.class, args);
     }
--- src/test/java/cn/xhb/blog/BlogApplicationTests.java
+++ src/test/java/cn/xhb/blog/BlogApplicationTests.java
@@ -3,7 +3,7 @@ package cn.xhb.blog;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class BlogApplicationTests {
 
     @Test
